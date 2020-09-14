a4e8c19ab3a674245ef5c859a00e0fc8126ec31b
unknown <viniciusrossmann@gmail.com>
2020-09-14 10:25:59 -0300

    adicionado comentarios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-server.listen(3000);
+server.listen(3000);//abre o servidor na porta 3000
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -19,44 +19,34 @@ io.on('connection', function (socket) {
         var beta = 1*msg.iniB;
 
         var pontos = msg.pontos;
-        var a = 1; 
-        var b = 0;
-        var erro = 0;
-        var erroTotal = 0;
-        var derErroA=0;
-        var derErroB=0;
+        var a = 1, b = 0
+        var erro = 0, erroTotal = 0; 
+        var derErroA=0, derErroB=0;
         var maxIte = 1*msg.maxIte;
 
         for(var i=1; i<maxIte+1; i++){
+            socket.emit('update-value', {a: a, b: b, iteracoes: i, erro: erroTotal});//manda os valores para serem renderizados
+            
             erro=0;
             erroTotal=0;
             derErroA=0;
             derErroB=0;
             
             pontos.forEach(function(item){
-                erro = ((((item.x*a)+b)-item.y));
+                erro = ((((item.x*a)+b)-item.y));//erro = valor de f(x) - valor correto de y
 
-                derErroA += (2*erro*item.x);
-                derErroB += (2*erro);
+                derErroA += (2*erro*item.x);//derivada do erro em relação ao coeficiente angular da reta
+                derErroB += (2*erro);//derivada do erro em relação ao coeficiente linear da reta
 
-                erroTotal += Math.pow(erro, 2);
+                erroTotal += erro;
             });
 
-            socket.emit('update-value', {a: a, b: b, iteracoes: i, erro: erroTotal});
+            a = a-(alpha*derErroA);//calcula o valor do coeficiente angular usando a constante alpha para normalizar
+            b = b-(beta*derErroB);//calcula o valor do coeficiente linear usando a constante beta para normalizar
 
-            a = a-(alpha*derErroA);
-            b = b-(beta*derErroB);
-
-            if(erroTotal==0) return;
+            if(erroTotal==0) return; //interrompe caso o erro seja nulo
         }
     });
 });
-
-
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-} 
 
 
